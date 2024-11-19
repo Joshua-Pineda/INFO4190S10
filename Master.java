3d6a9f373c26ca057bb1dde588604c86cb29b189
Joshua-Pineda <70978998+Joshua-Pineda@users.noreply.github.com>
2024-11-18 20:39:59 -0800

    master edits

--- Master.java
+++ Master.java
@@ -1,30 +1,23 @@
 
+
 public class Master {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] test = {4,3,5,6};
-		sort(test);
-		System.out.println(test);
-
-	}
+	
 
 	
-	public static void sort(int [] arrayOfInt) {
-		//iterate through array
-		for (int i = 0; i < arrayOfInt.length; i++) {
-			//this where we store the current element to temporarily take it out the array
-			int currentInt = arrayOfInt[i];
-			int k;
-			for (k = i-1; k >= 0 && arrayOfInt[k] > currentInt; k--) {
-				arrayOfInt[k+1] = arrayOfInt[k];
-			}
-			//insert the one thats taken out into k+1
-			arrayOfInt[k+1] = currentInt;
-		}
+	public int secondBig(int num []) {
+		biggestNumber b = new biggestNumber();
 		
+		int big = num[0];
+		int biggest = b.findBiggestNum(num);
+		for (int i = 0 ; i < num.length ; i++) {
+			if(num[i] > big && num[i] < biggest) {
+				big = num[i];
+			}
+	}
+		
+		return big;
 	}
 
 }
-
 
